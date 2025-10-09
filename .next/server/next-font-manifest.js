9151122b22973628f86a2b02526ce90cdad1e9c7
Emanuel Marquez <marquezema127@gmail.com>
2025-10-08 22:57:38 -0300

    Remove .next from repository

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{\"/\":[\"static/media/9dd75fadc5b3df29-s.p.woff2\",\"static/media/cf6384938887ffe2-s.p.woff2\"]},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":true}"
